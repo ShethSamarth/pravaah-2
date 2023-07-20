c55e3ea4ab4348f299ab1ad63c8aa7b5bb79d91e
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-07-20 18:18:52 +0530

    sanity category removed

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,9 @@
 import { type SchemaTypeDefinition } from "sanity"
 
 import blockContent from "./schemas/blockContent"
-import category from "./schemas/category"
 import events from "./schemas/events"
 import general from "./schemas/general"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContent, category, events, general],
+  types: [blockContent, events, general],
 }
